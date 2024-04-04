158274f6fd174dbcb73238c2d9a3479ace0ccf9c
devhindo <lildevhind@gmail.com>
2024-04-04 13:10:19 +0200

    init tense cmd

---
+++ cmd/tense.go
@@ -0,0 +1,5 @@
+package cmd
+
+func tense(url string, n int64) error {
+	return nil
+}
---
+++ cmd/url.go
@@ -0,0 +1,54 @@
+package cmd
+
+import (
+
+	"github.com/spf13/cobra"
+)
+
+var urlCmd = &cobra.Command{
+	Use:   "url",
+	Short: "tense a url",
+	Long:  `tense a url`,
+	Run:   tenseUrl,
+}
+
+func tenseUrl(cmd *cobra.Command, args []string) {
+	url, err := cmd.Flags().GetString("url")
+
+	if err != nil {
+		cmd.Help()
+		return
+	}
+
+	if url == "" {
+		cmd.Help()
+		return
+	}
+
+	n, err := cmd.Flags().GetInt64("number")
+
+	if err != nil {
+		cmd.Help()
+		return
+	}
+
+	if n <= 0 {
+		cmd.Help()
+		return
+	}
+
+	err = tense(url, n)
+	if err != nil {
+		cmd.Help()
+		return
+	}
+}
+
+func init() {
+	rootCmd.AddCommand(urlCmd)
+
+	urlCmd.Flags().StringP("url", "u", "", "url to tense")
+	urlCmd.Flags().Int64P("number", "n", 1, "number of times to tense the url")
+
+}
+
